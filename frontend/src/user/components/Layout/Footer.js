ffaa2adf8b3d82b291252391f09a018db106250f
Kien <Email ca bn>
2025-09-23 16:01:26 +0700

    Convert frontend from submodule to normal folder

---
+++ frontend/src/user/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Nội dung của đầu trang</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/user/pages/About.js
@@ -0,0 +1,1 @@
+export default function About() { return <h2>About</h2>; }
